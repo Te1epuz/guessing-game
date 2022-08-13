dda05a673c32b420cca2109af764e4d6c2cfafed
Alexander Martinovich <alexander_martinovich@msn.com>
2022-08-13 16:26:45 +0300

    feat: guessing game test complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,18 @@
 class GuessingGame {
     constructor() {}
-
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
     guess() {
-
+      let value = Math.round((this.max - this.min) / 2 + this.min);
+      return value;
     }
-
     lower() {
-
+      this.max = Math.round((this.max - this.min) / 2 + this.min);
     }
-
     greater() {
-
+      this.min = Math.round((this.max - this.min) / 2 + this.min);
     }
 }
 
